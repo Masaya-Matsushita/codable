eef234c3207e003a5f0086d345178b17d933fd66
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-04-02 17:30:49 +0900

    firebaseを導入

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiaQDxbIfNQC4f8cM0lGGLSXkpCq28zaY",
+  authDomain: "css-gallary.firebaseapp.com",
+  projectId: "css-gallary",
+  storageBucket: "css-gallary.appspot.com",
+  messagingSenderId: "863315383457",
+  appId: "1:863315383457:web:4a772384787d2114964ce0",
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
