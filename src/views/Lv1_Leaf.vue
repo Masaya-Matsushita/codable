845b0ca71edc96f05d38f47fe116a06c294719b6
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-04-03 04:56:59 +0900

    v-ace-editorを導入しました。

--- src/views/Lv1_Leaf.vue
+++ src/views/Lv1_Leaf.vue
@@ -1,3 +1,31 @@
 <template>
   <div>hello world</div>
+  <EditorArea class="editor" :passCode="code" @update:code="updateContent" />
 </template>
+
+<script>
+import EditorArea from "@/components/EditorArea.vue"
+
+export default {
+  components: {
+    EditorArea,
+  },
+  data() {
+    return {
+      code: "aiueo",
+    }
+  },
+  methods: {
+    updateContent(value) {
+      this.code = value
+    },
+  },
+}
+</script>
+
+<style scoped>
+.editor {
+  height: 600px;
+  width: 600px;
+}
+</style>
