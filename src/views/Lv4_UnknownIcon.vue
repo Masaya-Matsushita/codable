46804eaecce91fe4f2be8a1e46aaf9a0e2596931
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-04-03 12:18:45 +0900

    Lv.4 UnknownIcon を実装しました。

--- src/views/Lv4_UnknownIcon.vue
+++ src/views/Lv4_UnknownIcon.vue
@@ -1,3 +1,72 @@
 <template>
-  <div>hello world</div>
+  <GameLayout
+    :setTitle="title"
+    :setCode="code"
+    :setSample="sample"
+    :setColorCodes="colorCodes"
+    @returnCode="updateContent"
+  />
 </template>
+
+<script>
+import GameLayout from "@/components/GameLayout.vue"
+
+export default {
+  components: {
+    GameLayout,
+  },
+  data() {
+    return {
+      title: "Lv.4 Unknown Icon",
+      code: `<div class="object"></div>
+
+<style>
+  .object{
+    width: 150px;
+    height: 150px;
+    background-color: #808080;
+    /*ここにコードを追加*/
+  }
+  .back{
+
+  }
+</style>`,
+      sample: `<div class="sample-body"></div>
+    <div class="sample-head"></div>
+
+<style>
+      .sample-head {
+        background-color: #808080;
+        width: 120px;
+        height: 120px;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -160%);
+        border-radius: 50%;
+        border: 15px solid #f0f8ff;
+      }
+      .sample-body {
+        background-color: #808080;
+        width: 180px;
+        height: 100px;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, 0);
+        border-radius: 50% 50% 0 0;
+      }
+      .sample-back{
+        background-color:#f0f8ff;
+      }
+    </style>`,
+      colorCodes: ["#808080", "#f0f8ff"],
+    }
+  },
+  methods: {
+    updateContent(value) {
+      this.code = value
+    },
+  },
+}
+</script>
