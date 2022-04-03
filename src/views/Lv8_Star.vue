c6383fd7b91a913a78403eb3fa9a8eba2d1c183a
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-04-03 13:35:28 +0900

    Lv8_Starを実装しました。

--- src/views/Lv8_Star.vue
+++ src/views/Lv8_Star.vue
@@ -17,38 +17,57 @@ export default {
   },
   data() {
     return {
-      title: "Lv.1 Leaf",
+      title: "Lv.8 Star",
       code: `<div class="object"></div>
 
 <style>
   .object{
     width: 200px;
     height: 200px;
-    background-color: #008000;
+    background-color: #ffd700;
     /*ここにコードを追加*/
   }
   .back{
 
   }
 </style>`,
-      sample: `<div class="sample-object"></div>
+      sample: `<div class="sample-star"></div>
 
 <style>
-  .sample-object{
-    width: 150px;
-    height: 150px;
-    background-color: #008000;
-    border-radius: 100px 0;
+  .sample-star {
+    border-left: 100px solid transparent;
+    border-right: 100px solid transparent;
+    border-bottom: 70px solid #ffd700;
+    height: 0;
+    width: 0;
     position: relative;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%) rotate(35.4deg);
+  }
+  .sample-star:before,
+  .sample-star:after {
+    border-left: 100px solid transparent;
+    border-right: 100px solid transparent;
+    border-bottom: 70px solid #ffd700;
+    content: "";
+    height: 0;
+    left: -100px;
+    position: absolute;
+    top: 0;
+    width: 0;
+  }
+  .sample-star:before {
+    transform: rotate(71deg);
+  }
+  .sample-star:after {
+    transform: rotate(-71deg);
   }
   .sample-back{
-    background-color: #fafad7;
-    }
+  background-color: #4682b4;
+  }
 </style>`,
-      colorCodes: ["#008000", "#fafad7"],
+      colorCodes: ["#ffd700", "#4682b4"],
     }
   },
   methods: {
