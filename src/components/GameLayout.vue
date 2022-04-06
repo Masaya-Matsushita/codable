6fe23b5de62838216f5663af9ba85d07cd6e5920
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-04-06 14:00:15 +0900

    GameLayoutをレスポンシブしている途中です。

--- src/components/GameLayout.vue
+++ src/components/GameLayout.vue
@@ -8,31 +8,36 @@
       lang="html"
       theme="chrome"
     />
-    <div class="canvas-area">
-      <div class="over-lap">
-        <CanvasArea class="back" :passCode="setCode" />
-        <SampleArea
-          class="sample-back upper"
-          :passSample="setSample"
-          :style="{
-            opacity: opaValue,
-          }"
+    <div class="display-area">
+      <div class="canvas-area">
+        <div class="over-lap">
+          <CanvasArea class="back" :passCode="setCode" />
+          <SampleArea
+            class="sample-back"
+            :passSample="setSample"
+            :style="{
+              opacity: opaValue,
+            }"
+          />
+        </div>
+        <OpacityBar
+          class="opacity-bar"
+          :passOpaValue="opaValue"
+          @returnOpaValue="updateOpacity"
         />
       </div>
-      <OpacityBar
-        class="opacity-bar"
-        :passOpaValue="opaValue"
-        @returnOpaValue="updateOpacity"
-      />
-    </div>
-    <div class="sample-area">
-      <SampleArea class="sample-back" :passSample="setSample" />
-      <ColorPalette class="color-palette" :passColorCodes="setColorCodes" />
+      <div class="sample-area">
+        <SampleArea class="sample-back" :passSample="setSample" />
+        <ColorPalette class="color-palette" :passColorCodes="setColorCodes" />
+      </div>
     </div>
   </div>
   <!-- ボタンがコンポーネント化できない
     https://v3.ja.vuejs.org/guide/component-basics.html#%E5%AD%90%E3%82%B3%E3%83%B3%E3%83%9B%E3%82%9A%E3%83%BC%E3%83%8D%E3%83%B3%E3%83%88%E3%81%AE%E3%82%A4%E3%83%98%E3%82%99%E3%83%B3%E3%83%88%E3%82%92%E8%B3%BC%E8%AA%AD%E3%81%99%E3%82%8B -->
-  <button class="howTo-button" @click="howToAlert">{{ howTo }}</button>
+  <button class="howTo-button" @click="howToAlert">
+    <img class="howTo-icon" :src="howTo.iconPath" :alt="howTo.iconAlt" />
+    {{ howTo.text }}
+  </button>
   <button class="finish-button" @click="finishGame">{{ finish }}</button>
 </template>
 
@@ -62,7 +67,11 @@ export default {
   data() {
     return {
       opaValue: 0.5,
-      howTo: "遊び方",
+      howTo: {
+        text: "遊び方",
+        iconPath: require("@/assets/HowToIcon.png"),
+        iconAlt: "?",
+      },
       finish: "完成",
     }
   },
@@ -115,7 +124,7 @@ export default {
       })
       await Queue.fire({
         title: "遊び方 3/4",
-        text: "正解のコードは1つとは限らないよ! 色々な方法を模索してみよう!",
+        text: "「透過度バー」や「カラーコード」をうまく利用しよう！",
         imageUrl: "https://unsplash.it/400/200",
         imageWidth: 400,
         imageHeight: 200,
@@ -171,6 +180,13 @@ export default {
   width: 600px;
   font-size: 1rem;
 }
+.display-area {
+  display: flex;
+  padding: 30px;
+}
+.sample-area {
+  margin-left: 10px;
+}
 .over-lap * {
   position: absolute;
 }
@@ -186,9 +202,6 @@ export default {
   outline: 1px solid #303030;
   padding: 10px;
 }
-.upper {
-  z-index: 1;
-}
 .opacity-bar {
   width: 350px;
   margin-top: 380px;
@@ -201,9 +214,44 @@ export default {
   width: 250px;
   font-size: 1.5rem;
 }
+.howTo-icon {
+  width: 1.2em;
+  transform: translate(-50%, 20%);
+}
 .finish-button {
   height: 80px;
   width: 250px;
   font-size: 1.5rem;
+}
+
+@media (max-width: 1200px) {
+  .editor {
+    outline: 1px solid #303030;
+    margin-right: 10px;
+  }
+  .display-area {
+    padding: 30px 0;
+  }
+}
+
+@media (max-width: 1090px) {
+  .content-area {
+    flex-direction: column-reverse;
+    align-items: center;
+  }
+}
+@media (max-width: 740px) {
+  .display-area {
+    flex-direction: column-reverse;
+  }
+  .sample-area {
+    margin: 0 0 30px 0;
+  }
+}
+@media (max-width: 620px) {
+  .editor {
+    width: 96vw;
+    padding: 0 2vw;
+  }
 }
 </style>
